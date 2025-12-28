7254e7864ed346b67006d9c558e67454c09ed1ff
zangrujie <3022068494@qq.com>
2025-12-28 18:28:31 +0800

    Lab6练习0

--- kern/schedule/default_sched.c
+++ kern/schedule/default_sched.c
@@ -18,8 +18,6 @@ static void
 RR_init(struct run_queue *rq)
 {
     // LAB6: YOUR CODE
-    list_init(&(rq->run_list));  // 初始化运行队列的链表
-    rq->proc_num = 0;             // 初始进程数为0
 }
 
 /*
@@ -37,13 +35,6 @@ static void
 RR_enqueue(struct run_queue *rq, struct proc_struct *proc)
 {
     // LAB6: YOUR CODE
-    assert(list_empty(&(proc->run_link)));           // 确保进程不在其他队列中
-    list_add_before(&(rq->run_list), &(proc->run_link));  // 加入队列尾部
-    if (proc->time_slice == 0 || proc->time_slice > rq->max_time_slice) {
-        proc->time_slice = rq->max_time_slice;       // 设置时间片
-    }
-    proc->rq = rq;                                   // 设置进程所属队列
-    rq->proc_num++;                                  // 队列进程数加1
 }
 
 /*
@@ -57,9 +48,6 @@ static void
 RR_dequeue(struct run_queue *rq, struct proc_struct *proc)
 {
     // LAB6: YOUR CODE
-    assert(!list_empty(&(proc->run_link)) && proc->rq == rq);  // 确保进程在队列中
-    list_del_init(&(proc->run_link));  // 从队列中移除
-    rq->proc_num--;                     // 队列进程数减1
 }
 
 /*
@@ -71,11 +59,6 @@ RR_dequeue(struct run_queue *rq, struct proc_struct *proc)
  * hint: see libs/list.h for routines of the list structures.
  */
 static struct proc_struct *
-    list_entry_t *le = list_next(&(rq->run_list));  // 获取队列头部的下一个节点
-    if (le != &(rq->run_list)) {                     // 队列非空
-        return le2proc(le, run_link);                 // 返回对应的进程
-    }
-    return NULL;  // 队列为空返回NULL
 RR_pick_next(struct run_queue *rq)
 {
     // LAB6: YOUR CODE
@@ -87,12 +70,6 @@ RR_pick_next(struct run_queue *rq)
  * exhausted and update the proc struct ``proc''. proc->time_slice
  * denotes the time slices left for current process. proc->need_resched
  * is the flag variable for process switching.
-    if (proc->time_slice > 0) {      // 如果时间片还有剩余
-        proc->time_slice--;           // 时间片减1
-    }
-    if (proc->time_slice == 0) {      // 时间片用完
-        proc->need_resched = 1;       // 设置需要重新调度标志
-    }
  */
 static void
 RR_proc_tick(struct run_queue *rq, struct proc_struct *proc)
